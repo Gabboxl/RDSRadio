9806609b682435f4fb6528e97f0f113f57dec52c
Gabriele <gabboxl0@gmail.com>
2019-09-07 22:01:02 +0200

    fix stream link

--- rdsradio.php
+++ rdsradio.php
@@ -141,7 +141,7 @@ class EventHandler extends \danog\MadelineProto\EventHandler
 
       file_put_contents('omg.sh', "#!/bin/bash \n mkfifo streams/$icsd.raw");
 
-      file_put_contents('figo.sh', '#!/bin/bash'." \n".'ffmpeg -i http://stream1.rds.it:8000/rds -vn -f s16le -ac 1 -ar 48000 -acodec pcm_s16le pipe:1 > streams/'."$icsd.raw"); //https://mediapolis.rai.it/relinker/relinkerServlet.htm?cont=2606803
+      file_put_contents('figo.sh', '#!/bin/bash'." \n".'ffmpeg -i https://icstream.rds.radio/rds -vn -f s16le -ac 1 -ar 48000 -acodec pcm_s16le pipe:1 > streams/'."$icsd.raw"); //https://mediapolis.rai.it/relinker/relinkerServlet.htm?cont=2606803
 
       shell_exec('chmod -R 0777 figo.sh omg.sh');
 
